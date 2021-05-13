6b1b12b19a2a74fe39802d25fc099e0457473371
Lex Houf <lexhoufio@gmail.com>
2021-05-13 12:36:59 +0200

    Fix angular 12 upgrade

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
-import 'zone.js/dist/zone-testing';
+import 'zone.js/testing';
 // eslint-disable-next-line import/order
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
