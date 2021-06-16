bf58e3e75af8ecde5802705c3ec007d37a19ce0f
IndyV <28678264+IndyV@users.noreply.github.com>
2021-06-16 23:03:37 +0200

    Add initial board display

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,5 +1,14 @@
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, Input } from '@angular/core';
+import firebase from 'firebase';
+
+import { IdentifiableModel } from '../../models/identifiable.model';
+import { SprintModel } from '../../models/sprint.model';
+import { StateModel } from '../../models/state.model';
+import { TaskModel } from '../../models/task.model';
+import { UserModel } from '../../models/user.model';
+
+import Timestamp = firebase.firestore.Timestamp;
 
 @Component({
   selector: 'app-board',
@@ -8,12 +17,20 @@ import { Component, Input } from '@angular/core';
 })
 export class BoardComponent {
   @Input()
-  public set lanes(value: string[]) {
-    this.board = ['Unassigned', ...value].reduce((acc, lane) => ({
-      ...acc,
-      [lane]: this.board[lane] ?? []
-    }), {})
-  }
+  public activeSprint?: IdentifiableModel<SprintModel>;
+  @Input()
+  public states!: IdentifiableModel<StateModel>[];
+  @Input()
+  public tasks!: IdentifiableModel<TaskModel>[];
+  @Input()
+  public lanes!: IdentifiableModel<UserModel>[];
+  
+  // public set lanes(value: string[]) {
+  //   this.board = ['Unassigned', ...value].reduce((acc, lane) => ({
+  //     ...acc,
+  //     [lane]: this.board[lane] ?? []
+  //   }), {})
+  // };
 
   public board: Record<string, string[]> = {
     Unassigned: [
@@ -24,8 +41,32 @@ export class BoardComponent {
     ]
   };
 
-  public get lanes() {
-    return Object.keys(this.board);
+  // public get lanes() {
+  //   return Object.keys(this.board);
+  // }
+
+  public get currSprint() {
+    return this.activeSprint;
+  }
+
+  public getAssigneeTask(user?: IdentifiableModel<UserModel>, state?: IdentifiableModel<StateModel>) {
+    return this.tasks.filter((task) => (task.assignee === user?.id) && task.state === (state?.id ?? ''))
+  }
+
+  public get statesInProject() {
+    return this.states.sort((a, b) => a.order - b.order)
+  }
+
+  public userDrop(event: CdkDragDrop<IdentifiableModel<UserModel>>) {
+    console.log(event.container.data);
+    console.log(event.previousContainer.data);
+    console.log(event.previousIndex);
+    console.log(event.currentIndex);
+    console.log(event.previousContainer === event.container);
+  }
+
+  public taskDrop(event: CdkDragDrop<IdentifiableModel<TaskModel>[], any>) {
+    console.log(event);
   }
 
   public drop(event: CdkDragDrop<string[]>) {
@@ -34,5 +75,13 @@ export class BoardComponent {
     } else {
       transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
     }
+  }
+
+  public getDate(timeStamp?: Timestamp) {
+    return timeStamp?.toDate()?.toLocaleDateString('nl-NL');
+  }
+
+  public resolveUser(assignee?: string) {
+    return this.lanes.filter((user) => user.id === assignee)[0]?.name ?? 'unassigned';
   }
 }
--- src/app/pages/project-board/project-board.component.ts
+++ src/app/pages/project-board/project-board.component.ts
@@ -5,8 +5,15 @@ import { map, switchMap } from 'rxjs/operators';
 
 import { IdentifiableModel } from '../../models/identifiable.model';
 import { ProjectModel } from '../../models/project.model';
+import { SprintModel } from '../../models/sprint.model';
+import { StateModel } from '../../models/state.model';
+import { TaskModel } from '../../models/task.model';
+import { UserModel } from '../../models/user.model';
 import { filterNullish } from '../../operators/filter-nullish';
 import { ProjectService } from '../../services/project.service';
+import { SprintService } from '../../services/sprint.service';
+import { StateService } from '../../services/state.service';
+import { TaskService } from '../../services/task.service';
 import { UserService } from '../../services/user.service';
 
 @Component({
@@ -16,20 +23,29 @@ import { UserService } from '../../services/user.service';
 })
 export class ProjectBoardComponent {
   public project$: Observable<IdentifiableModel<ProjectModel> & {
-    lanes: string[]
+    states: IdentifiableModel<StateModel>[],
+    activeSprint?: IdentifiableModel<SprintModel>,
+    tasks: IdentifiableModel<TaskModel>[],
+    lanes: IdentifiableModel<UserModel>[]
   }>;
 
-  public constructor(route: ActivatedRoute, projectService: ProjectService, userService: UserService) {
+  public constructor(route: ActivatedRoute, projectService: ProjectService, stateService: StateService, taskService: TaskService, sprintService: SprintService, userService: UserService) {
     this.project$ = route.parent!.paramMap.pipe(
       switchMap((params) => projectService.getProject(params.get('project'))),
       filterNullish(),
       switchMap((project) => combineLatest([
         of(project),
+        stateService.getStates(project),
+        taskService.getTasks(project),
+        sprintService.getCurrentSprint(project),
         userService.getUsers(project)
       ])),
-      map(([project, users]) => ({
+      map(([project, states, tasks, activeSprint, users]) => ({
         ...project,
-        lanes: users.filter((user) => project.users.indexOf(user.id) !== -1).map((user) => user.name)
+        states,
+        activeSprint,
+        tasks: tasks.filter((task) => task.sprint === project.currentSprint),
+        lanes: users.filter((user) => project.users.indexOf(user.id) !== -1)
       }))
     );
   }
