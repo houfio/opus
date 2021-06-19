6ea97ea733f4823cd15f4ebaa111cf2efdfea7f1
IndyV <28678264+IndyV@users.noreply.github.com>
2021-06-19 19:33:32 +0200

    Update cypress firebase config

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -4,9 +4,17 @@ import "firebase/auth";
 import "firebase/database";
 import "firebase/firestore";
 
-import { environment } from '../../src/environments/environment';
-
-firebase.initializeApp(environment.firebase);
+firebase.initializeApp(
+  {
+    apiKey: 'AIzaSyCm8fqcHWtVKaqb9Z6yml8pAVI3OZ1MDG0',
+    authDomain: 'burgerbot-fdc33.firebaseapp.com',
+    databaseURL: 'https://burgerbot-fdc33.firebaseio.com',
+    projectId: 'burgerbot-fdc33',
+    storageBucket: 'burgerbot-fdc33.appspot.com',
+    messagingSenderId: '11843797210',
+    appId: '1:11843797210:web:b1441631c7ec0f808fa5e3'
+  }
+);
 
 attachCustomCommands({ Cypress, cy, firebase });
 
